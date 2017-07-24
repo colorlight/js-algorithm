4baf84e5322054eed6e2ca7dc38bfb729bf59510
colorlight <colorlightleecaa@gmail.com>
2017-07-24 16:11:57 +0800

    选择排序

---
+++ 选择排序.js
@@ -0,0 +1,24 @@
+function select(arr){
+  function findMin(curArr){
+    let min = curArr[0];
+    let index = 0;
+    curArr.forEach(function(v,i){
+      if(v < min){
+        min = v
+        index = i;
+      }
+    })
+    return index;
+  }
+
+  function swap(curArr, from, to){
+    let temp = curArr[from]
+    curArr[from] = to
+    curArr[to] = temp
+  }
+
+  for(let i = 0, len = arr.length; i < len; i++){
+    let minIndex = findMin(arr.slice(i)) + i;
+    swap(arr, i, minIndex);
+  }
+}
